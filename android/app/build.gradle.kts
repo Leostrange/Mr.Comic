43850efb559b036f5d385b0cd5845c9619ca8c64
Leostrange <xmetalcore@outlook.com>
2026-07-27 21:38:57 +0700

    docs: publish 2.2.0 changelog and clean repository media

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "io.leostrange.mrcomic"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "2.1.0"
+        versionCode = 3
+        versionName = "2.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
