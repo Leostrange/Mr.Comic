a40520a4aa15e9af0139262addfa4c37d1b0d92a
Leostrange <1.68255685e+08+Leostrange@users.noreply.github.com>
2026-08-16 20:10:55 +0000

    fix: enable desugaring for Readium module

--- android/engine-epub-readium/build.gradle.kts
+++ android/engine-epub-readium/build.gradle.kts
@@ -22,6 +22,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -48,6 +49,7 @@ dependencies {
     implementation(libs.readium.navigator)
 
     ksp(libs.google.hilt.compiler)
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 
     testImplementation(libs.test.junit)
     testImplementation(libs.robolectric)
