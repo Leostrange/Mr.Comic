7776d31c1aa1fc19c7e738cbf84d1dc735d1260f
Leostrange <1.68255685e+08+Leostrange@users.noreply.github.com>
2026-08-17 00:52:05 +0000

    test: add runner dependencies to remaining modules

--- android/core-ui/build.gradle.kts
+++ android/core-ui/build.gradle.kts
@@ -31,4 +31,6 @@ dependencies {
     implementation(libs.coil.compose)
     testImplementation(libs.test.junit)
     debugImplementation("androidx.compose.ui:ui-tooling")
+    androidTestImplementation(libs.test.androidx.runner)
+    androidTestImplementation(libs.test.androidx.junit)
 }
